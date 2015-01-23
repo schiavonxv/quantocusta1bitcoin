40f9354f957623f3ebd5d323fa57d830c99e4c13
Fernando Paladini <fnpaladini@gmail.com>
2015-01-23 10:17:24 -0200

    Preparando para Produção

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'static#home'
+  #root 'static#home'
 
   # POST /api/v1/cotacao
   # API da cotação do Bitcoin na Foxbit, que será acessada pelo robô e pelo próprio site para fornecer a cotação ao usuário.
