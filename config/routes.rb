9fd7f6fb0695a2f34fc7d429c59fba18e250ba97
Fernando Paladini <fnpaladini@gmail.com>
2015-01-25 06:58:18 -0200

    Rotas adicionadas para o site + primeiros edições no CSS para Mobile]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  #root 'static#home'
+  # Rota padrão para a página inicial do site.
+  root 'static#home'
 
   # POST /api/v1/cotacao
   # API da cotação do Bitcoin na Foxbit, que será acessada pelo robô e pelo próprio site para fornecer a cotação ao usuário.
